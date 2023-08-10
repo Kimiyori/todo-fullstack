0594ed241b972c653340d559c21e0925e2cf1f36
Maxim <maksimkalin@mail.ru>
2023-08-10 22:24:09 +0300

    feat: add env

--- frontend/src/api/meta.ts
+++ frontend/src/api/meta.ts
@@ -2,5 +2,5 @@ import { Taxios } from '@simplesmiler/taxios';
 import axios from 'axios';
 import { ToDo } from './ToDo';
 
-const taxios = new Taxios<ToDo>(axios.create({ baseURL: 'http://localhost:8000' }));
+const taxios = new Taxios<ToDo>(axios.create({ baseURL: process.env.REACT_APP_BACKEND_URL }));
 export default taxios;
